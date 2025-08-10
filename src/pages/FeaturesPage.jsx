0a5a2afdc67a3f1ec4bf574326817d9942e0a725
saharacasey <caseysahara@outlook.com>
2025-08-09 22:36:52 -0400

    Delete src/pages/FeaturesPage.jsx

--- src/pages/FeaturesPage.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function FeaturesPage(){
-  return <main><h1>Features</h1><ul><li>Climate-responsive massing</li><li>Material intelligence</li></ul></main>
-}
